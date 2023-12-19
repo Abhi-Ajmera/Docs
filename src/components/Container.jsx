aad414d288dbf04f5fe1a6c6acc7f6d19dd696e4
Abhishek Ajmera <abhishekajmera17@gmail.com>
2023-12-19 22:42:06 +0530

    complete

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -16,10 +16,10 @@ const Container = () => {
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [docs]);
 
   return (
-    <div className='fixed h-full z-[3] flex flex-shrink-0 w-full mx-4 gap-4 overflow-scroll no-scrollbar flex-wrap mb-4 max-sm:justify-center max-sm:mx-0'>
+    <div className='fixed z-[3] flex flex-shrink-0 w-full mx-4 gap-4 overflow-scroll no-scrollbar flex-wrap mb-4 max-sm:justify-center max-sm:mx-0'>
       {docs.map((doc) => (
         <Card
           key={doc.id}
