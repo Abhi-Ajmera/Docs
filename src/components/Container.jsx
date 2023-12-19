a027ac1e1b5e42f3e1d1952908128be9fb3d8579
Abhishek Ajmera <abhishekajmera17@gmail.com>
2023-12-19 22:58:58 +0530

    done

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -19,7 +19,7 @@ const Container = () => {
   }, [docs]);
 
   return (
-    <div className='fixed z-[3] flex flex-shrink-0 w-full mx-4 gap-4 overflow-scroll no-scrollbar flex-wrap mb-4 max-sm:justify-center max-sm:mx-0'>
+    <div className='fixed z-[3] my-0 h-full flex flex-shrink-0 items-start w-full mx-4 gap-4 overflow-scroll no-scrollbar flex-wrap mb-4 max-sm:justify-center max-sm:mx-0'>
       {docs.map((doc) => (
         <Card
           key={doc.id}
